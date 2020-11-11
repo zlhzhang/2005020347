594b6107d7faf45aa7783d4975e3308de8d5b666
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-11 17:34:16 +0800

    Create 002.c

---
+++ 002.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a[]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int i;int n;
+	for(i=0;i<=11;i++)
+	{printf("%d\n\n",a[i]);}
+	scanf("%d",&n);
+
+	printf("这个月的天数是%d天\n",a[n-1]);
+	
+	
+	
+	
+	//while(1){
+	//printf("zaoan\n");
+	}
