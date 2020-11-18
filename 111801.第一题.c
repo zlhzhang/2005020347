d25468edebd79693061acc3f359a77b2e3b809c5
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-18 08:58:26 +0800

    Create 111801.第一题.c

---
+++ 111801.第一题.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a=3,b=4,c=5,x=3,y=4;
+if(a+b>c&&b==c)
+	printf("1\n");
+else
+    printf("0\n");
+if(a||b+c&&b-c)
+	printf("1\n");
+else
+	printf("0\n");
+if(!(a>b)&&!c||1)
+	printf("1\n");
+else
+	printf("0\n");
+if(!(x=a)&&(y=b)&&0)
+	printf("1\n");
+else
+	printf("0\n");
+if(!(a+b)+c-1&&b+c/2)
+	printf("1\n");
+else
+	printf("0\n");
+
+}
