f431eef8481247b6aa1a097337a856dea2dc27e2
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-18 16:44:48 +0800

    Update 111802.程序员最牛逼.c

--- 111802.程序员最牛逼.c
+++ 111802.程序员最牛逼.c
@@ -2,20 +2,23 @@
 int main()
 {
 	int n;
-	printf("                                  打工人工资等级划分\n"); 
-        scanf("%d",&n);
+   printf("                                  打工人工资等级划分\n");
+scanf("%d",&n);
 if(n<=-1)
 	printf("你还有花呗没还，无法使用该程序。\n");
+
 else if(0<=n&&n<=500)
 	printf("赤贫。\n");
 else if(501<=n&&n<=1500)
 	printf("贫困。\n");
 else if(1501<=n&&n<=3000)
 	printf("普通打工人。\n");
-else if(3001<=n&&n<=12000)
+else if(3001<=n&&n<=5000)
+	printf("普通清洁工。\n");
+else if(5001<=n&&n<=12000)
 	printf("水电工工资。\n");
 else if(12001<=n&&n<=50000)
 	printf("脑袋大脖子粗不是老板就是伙夫。\n");
-else if(50001<=n&&n<=20000000)
+else if(50001<=n&&n<=2000000000000)
 	printf("程序员。\n");
 }
