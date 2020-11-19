5bfb5c7396c32fadefc41984832208eba87a40d4
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-19 21:05:55 +0800

    Update 111920.图形.c

--- 111920.图形.c
+++ 111920.图形.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	printf("     ★\n");
-	printf("    ★★\n");
-	printf("   ★★★\n");
-	printf("  ★★★★\n");
+	printf("     ★\n"    );
+	printf("    ★★\n"   );
+	printf("   ★★★\n"  );
+	printf("  ★★★★\n" );
 	printf(" ★★★★★\n");
-
 }
