06a7c84e8aee3e2502ae3e8f42c94250e7eb300b
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-19 21:03:51 +0800

    Update 111910.性别.c

--- 111910.性别.c
+++ 111910.性别.c
@@ -10,3 +10,19 @@ printf("女\n");
 	else if(i!=0&&i!=1)
 printf("输入有误，请重新输入。\n");
 }
+
+
+
+
+#include<stdio.h>
+int main()
+{
+	int i;
+scanf("%d",&i);
+	if(i==1)
+printf("男\n");
+	else if(i==0)
+printf("女\n");
+	else 
+printf("输入有误，请重新输入。\n");
+}
