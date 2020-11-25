ffdd8c1762634df2ed5a3dde18ad87ddce8982ce
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-25 16:49:20 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("                                QS世界大学排名查询  \n");
+	scanf("%d",&n);
+if(n==1)
+	printf("麻省理工学院\n");
+else if(n==2)
+	printf("斯坦福大学。\n");
+else if(n==3)
+	printf("哈佛大学。\n");
+else if(n==4)
+	printf("牛津大学。\n");
+else if(n==5)
+	printf("加州理工 。\n");
+else if(n==6)
+	printf("苏黎世联邦工业大学。\n");
+else if(n==7)
+	printf("剑桥大学。\n");
+else if(n==8)
+	printf("伦敦大学学院\n");
+else if(n==9)
+	printf("帝国理工学院\n");
+else if(n==10)
+	printf("芝加哥大学\n");
+else
+	printf("老铁排名太靠后了，没有你啊！");
+}
