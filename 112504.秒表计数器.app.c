61e39d40caa150fb326cc1395538379e87663219
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-11-26 11:59:26 +0800

    Update 112504.秒表计数器.app.c

--- 112504.秒表计数器.app.c
+++ 112504.秒表计数器.app.c
@@ -4,10 +4,33 @@ int main()
 {	
 	int a=1;
 	while (1)
-	{
+	{       
+	        Sleep(1000);
 		printf("现在是%d秒\n",a++);
-		Sleep(1000);
+		
 	}
 	system("pause");
 	return 0;
 }
+
+
+
+
+
+
+
+
+/*#include<stdio.h>
+#include<windows.h>
+int main()
+{	
+	int a=0;
+	for (;;)
+	{
+		Sleep(1000);
+		printf("现在是%d秒\n",++a);
+
+	}
+	return 0;
+}
+/*
