6862d809ed7f11a2941509c7a96dd7355fd6bab9
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-12-02 11:06:26 +0000

    Update 120204.数据类型的转换.app.c

--- 120204.数据类型的转换.app.c
+++ 120204.数据类型的转换.app.c
@@ -5,3 +5,13 @@ int main()
     printf("%d",(int) n);
     return 0;
 }
+
+
+
+//#include<stdio.h>
+//int main()
+//{   double n=3.14;
+//    scanf("%lf",&n);
+//    printf("%d",(int) n);
+//    return 0;
+//}
