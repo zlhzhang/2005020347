0485acbdd39cc2e40e709ecbfce4e0ee27e832c2
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-12-09 17:34:56 +0800

    Create 120906.吃货给我安排.app.c

---
+++ 120906.吃货给我安排.app.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char n[30];
+	scanf("%s",&n);
+if(strcmp(n,"春节")==0)
+	{
+	printf("给我安排饺子\n");
+	}
+if(strcmp(n,"端午节")==0)
+	{
+	printf("给我安排粽子\n");
+	}
+if(strcmp(n,"元宵节")==0)
+	{
+	printf("给我安排元宵\n");
+	}
+if(strcmp(n,"中秋节")==0)
+	{
+	printf("给我安排月饼\n");
+	}
+
+}		
+
+	
