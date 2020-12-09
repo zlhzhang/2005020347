319a1a986f884fdbd8b88ba4f20ff574d1dd1713
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-12-09 16:51:17 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char n[]="he  wei  jun  shi  sha   bi";
+	char m[]="he  wei  jun  shi  sha   bi";
+	if(strcmp(n,m)==0)
+	{
+		printf("相同\n");
+	}
+	else
+	{
+		printf("不相同\n");
+	}
+	
+}	
