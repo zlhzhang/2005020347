41407551c3d1349f34b35b6eeaf4857404ff495d
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-12-10 19:30:10 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -8,19 +8,18 @@ if(strcmp(n,"春节")==0)
 	{
 	printf("给我安排饺子\n");
 	}
-if(strcmp(n,"端午节")==0)
+else if(strcmp(n,"端午节")==0)
 	{
 	printf("给我安排粽子\n");
 	}
-if(strcmp(n,"元宵节")==0)
+else if(strcmp(n,"元宵节")==0)
 	{
 	printf("给我安排元宵\n");
 	}
-if(strcmp(n,"中秋节")==0)
+else if(strcmp(n,"中秋节")==0)
 	{
 	printf("给我安排月饼\n");
 	}
 
 }		
-
 	
