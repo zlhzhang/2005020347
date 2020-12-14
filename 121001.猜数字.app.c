eec4aa61e427966375a163100c8f21896312ec45
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-12-14 20:59:11 +0800

    Update 121001.猜数字.app.c

--- 121001.猜数字.app.c
+++ 121001.猜数字.app.c
@@ -1,29 +1,48 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {	
-	
-	char n[30];
-	printf("输入口令才能玩游戏：wo shi shuai ge 的汉字。\n");
-	scanf("%s",&n);
-if( strcmp(n,"我是帅哥") ==0 )
-{
-	int k=6;
-	int a;
-	printf("                                            小霸王猜数字1-20之间\n");
-for(;;){	
-	scanf("%d",&a);
-if(a==k){
-	printf("恭喜你答对了\n");
+	int answer = 6;
+	char t;
+	int guess;
+	char n [30];
+	printf ("输入口令才能玩游戏：wo shi shuai ge 的汉字。\n");
+	for ( ; ; )
+{	
+	scanf ("%s",&n);
+if ( strcmp(n,"我是帅哥") ==0 )
+	{
+	printf ("恭喜你答对了，你就是帅哥，你可以玩游戏了，开始吧！\n\n\n");
 	break;
-		}
-else if(a>k){
-	printf("有点大\n");
-            }
-else if(a<k){
-	printf("有点小\n");
-            }
-         } 
+	}
+if ( strcmp(n,"我是帅哥") ) 
+	{
+	printf ("口令不正确，请重新输入！\n");
+	}
 }
-else
-	printf("名字错误");
+	//第一个for循环结束
+	printf ("                                            小霸王猜数字1-20之间\n");
+for ( ; ; )
+{	
+if(!scanf("%d", &guess)) 
+	{
+	scanf("%c", &t);
+	printf("请输入数字\n");
+	continue;
+	}
+if ( guess==answer )
+	{
+	printf ("恭喜你答对了\n");
+	break;
+	}
+else if ( guess>answer )
+	{
+	printf ("有点大\n\n");
+    }
+else if ( guess<answer )
+	{
+	printf ("有点小\n\n");
+    }
+} 
+//第二个for循环结束
 }
