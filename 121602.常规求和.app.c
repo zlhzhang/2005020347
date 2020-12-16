1d72eec414f4cb64ac61dc3b71e5e535622a8255
zlhzhang <74294777+zlhzhang@users.noreply.github.com>
2020-12-16 09:12:41 +0800

    Update 121602.常规求和.app.c

--- 121602.常规求和.app.c
+++ 121602.常规求和.app.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-int main(void) {
+int main(void) 
+{
 	int i;
 	int a = 0;
 	scanf("%d", &i);
-	for (; i >= 0; i--) {
-		a += i;
+	for (; i >= 0; i--){ 
+	a = a + i;
 	}
 	printf("%d\n", a);
 }
-
